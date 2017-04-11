0e4989b81320a78f56478e37dc7dff438753fb0f
Windstrider <joshowen1995@gmail.com>
2017-04-10 22:29:14 -0500

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,23 @@
+(function() {
+
+	  // Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyClwsYJMweZRPRq6uqIO5-Me0Q3Fr-Fxjg",
+	    authDomain: "database-29555.firebaseapp.com",
+	    databaseURL: "https://database-29555.firebaseio.com",
+	    projectId: "database-29555",
+	    storageBucket: "database-29555.appspot.com",
+	    messagingSenderId: "1087442343696"
+	  };
+	  firebase.initializeApp(config);
+    
+  //Get elemetns
+  const preObject = document.getElementById('object');
+  
+  //Create references
+  cosnt dbRefObject = firebase.database().ref().child('object');
+  
+  //Sync object changes
+  dbRef.Object.on('value', snap => console.log(snap.val()));
+  
+}());
