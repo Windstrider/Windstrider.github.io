828a7b1a30b36e523e7a12ae6364aa06f51a6b6d
Windstrider <joshowen1995@gmail.com>
2017-04-10 22:49:38 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 (function() {
+// Initialize Firebase
+ const config = {
+	apiKey: "AIzaSyClwsYJMweZRPRq6uqIO5-Me0Q3Fr-Fxjg",
+	authDomain: "database-29555.firebaseapp.com",
+	databaseURL: "https://database-29555.firebaseio.com",
+	projectId: "database-29555",
+	storageBucket: "database-29555.appspot.com",
+	messagingSenderId: "1087442343696"
+	};
+firebase.initializeApp(config);
     
   //Get elemetns
   const preObject = document.getElementById('object');
